5f8894e966ef0e1ec2dfc8423a7a87e403819d95
Dasun99Githendra <75695804+Dasun99Githendra@users.noreply.github.com>
2022-01-15 14:53:59 +0530

    Add files via upload

---
+++ lib/credentials.dart
@@ -0,0 +1,2 @@
+const apiKey = '1a1e1ce19d3f697cb292c68ba558eda5';
+//This is the api key.
---
+++ lib/screens/no_location.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+//If we can't get the location from net we can manually code that in here.
